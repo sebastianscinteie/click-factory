95973f900c27296f637df4cfae8da364300deb0c
Sebastian Scinteie <sebastian.scinteie@gmail.com>
2023-07-29 14:28:59 +0300

    Nice grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Image from 'next/image'
 
+function Square(){
+  return <>
+    <div className='bg-slate-400 rounded shadow-2xl m-10 w-60 h-60'></div>
+  </>
+}
+
 export default function Home() {
+
+  function squareBlock() {
+    let block = [];
+    let maxSquares;
+    for(let i = 0; i < 20; i++) {
+      block.push(<Square key={i}/>)
+    }
+
+    return block;
+  }
+
   return (
     <main>
-
+      <div className='flex justify-center'>
+        <div className='grid grid-cols-5 max-w-7xl min-w-max'>{squareBlock()}</div>
+      </div>
     </main>
   )
 }
