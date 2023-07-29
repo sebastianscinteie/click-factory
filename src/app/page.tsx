96338c94deec954ce5062d86a3cdb838cf523dd9
Sebastian Scinteie <sebastian.scinteie@gmail.com>
2023-07-29 16:39:07 +0300

    Add resource on click

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,57 @@
-import Image from 'next/image'
+'use client'
+
+import { useState } from "react";
 
 function Square(){
+  const [cyan, setCyan] = useState(0);
+  const [magenta, setMagenta] = useState(0);
+  const [yellow, setYellow] = useState(0);
+
+  function incrementResources() {
+    let [c, m, y] = [cyan, magenta, yellow]
+
+    if(c + 1 == 4) {
+      c = 0;
+      if(m + 1 == 4) {
+        m = 0;
+        y++;
+      } else {
+        m++;
+      }
+    } else {
+      c++;
+    }
+
+    setCyan(c);
+    setMagenta(m);
+    setYellow(y);
+  }
+
+  function buildResourceArray(){
+    let [c, m, y] = [cyan, magenta, yellow]
+    
+    const yArr = []
+    for (let i = 0; i < y; i++) {
+      yArr.push(<div className='bg-yellow-400 rounded-full shadow-xl w-5 h-5 m-1 p-3' key={i}></div>)
+    }
+
+    const mArr = []
+    for (let i = 0; i < m; i++) {
+      mArr.push(<div className='bg-purple-600 rounded-full shadow-xl w-5 h-5 m-1 p-3' key={y + i}></div>)
+    }
+
+    const cArr = []
+    for (let i = 0; i < c; i++) {
+      cArr.push(<div className='bg-cyan-400 rounded-full shadow-xl w-5 h-5 m-1 p-3' key={y + m + i}></div>)
+    }
+
+    return yArr.concat(mArr).concat(cArr);
+  }
 
   return <>
-    <div className='bg-slate-400 rounded-lg shadow-2xl m-10 p-5 w-60 h-60'>
+    <div className='bg-slate-400 rounded-lg shadow-2xl m-10 p-5 w-60 h-60' onClick={incrementResources}>
       <div className='inline-grid grid-cols-5'>
-        <div className='bg-cyan-400 rounded-full shadow-xl w-5 h-5 m-1 p-3'></div>
-        <div className='bg-purple-600 rounded-full shadow-xl w-5 h-5 m-1 p-3'></div>
-        <div className='bg-yellow-400 rounded-full shadow-xl w-5 h-5 m-1 p-3'></div>
+        {buildResourceArray()}
       </div>
     </div>
   </>
